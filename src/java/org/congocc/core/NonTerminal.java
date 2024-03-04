086d3303f8c4941abd4243f3367840c51c9a2125
revusky <revusky@gmail.com>
2024-03-04 12:26:22 +0100

    Fixing issue raised by hylkevds at https://discuss.congocc.org/d/54-lexical-state-swiching-scan-and-odd-error-messages

--- src/java/org/congocc/core/NonTerminal.java
+++ src/java/org/congocc/core/NonTerminal.java
@@ -110,31 +110,15 @@ public class NonTerminal extends Expansion implements SyntaxElement {
         return getNestedExpansion().isSingleTokenLookahead();
      }
 
-     private boolean checkNestedExpansion() {
-         Expansion nested = getNestedExpansion();
-         if (nested instanceof ExpansionSequence) {
-             for (Expansion sub : nested.childrenOfType(Expansion.class)) {
-                 if (!(sub instanceof NonTerminal)) {
-                     // KLUDGE? For now we don't nest further into nonterminals
-                     // It seems like we should, but the code blows up. I need
-                     // to revisit this! For now, it's "good enough for government work, I suppose."
-                     if (sub.startsWithLexicalChange()) return true;
-                 }
-                 if (!sub.isPossiblyEmpty()) break;
-             }
-         }
-         return false;
-     }
-
      public boolean startsWithLexicalChange() {
         if (getProduction().getLexicalState() != null) return true;
-        return checkNestedExpansion();
+        return getNestedExpansion().startsWithLexicalChange();
      }
 
      public boolean startsWithGlobalCodeAction() {
         CodeBlock javaCode = getProduction().getJavaCode();
         if (javaCode != null && javaCode.isAppliesInLookahead()) return true;
-        return checkNestedExpansion();
+        return getNestedExpansion().startsWithGlobalCodeAction();
      }
 
      @Override
