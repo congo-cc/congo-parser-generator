bfd9894e89ede8b918db9bede148bd1074854d29
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2024-04-16 12:35:40 +0100

    Fix #158: Refine tests to ensure C# directive error handling works.

---
+++ tests/unparsed/invalid2.cs
@@ -0,0 +1,11 @@
+#pragma warn disable 999
+
+####
+
+namespace foo.bar {
+#if true
+    /* This is a comment. */
+#else
+    // This is another.
+#endif
+}
---
+++ tests/unparsed/invalid3.cs
@@ -0,0 +1,10 @@
+#pragma warn disable 999
+
+#
+namespace foo.bar {
+#if true
+    /* This is a comment. */
+#else
+    // This is another.
+#endif
+}
