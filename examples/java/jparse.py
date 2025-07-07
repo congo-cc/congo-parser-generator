abb033df373f3108baa9c68c95edd2830c14d967
revusky <revusky@gmail.com>
2025-07-07 12:23:26 +0200

    Adding a python parsing test for the Java and CSharp. Still need to have similar thing going for CSharp. And also for the lua and python grammars. But we also need to figure out a way to generalize this so that we're not repeating ourselves constantly

--- examples/java/jparse.py
+++ examples/java/jparse.py
@@ -5,21 +5,28 @@
 #
 import argparse
 import glob
-import logging
 import os
 import sys
+import time
+import traceback
 
 from javaparser import Parser
 
-# DEBUGGING = 'PY_DEBUG' in os.environ
-DEBUGGING = True
-
-logger = logging.getLogger(__name__)
+successes = failures = 0;
+start = time.time()
 
 def parse_file(fn):
+    global successes, failures
     parser = Parser(fn)
-    parser.parse_Root()
-    print('Parser in Python successfully parsed %s' % fn)
+    try :
+        parser.parse_Root()
+        successes+=1
+        print('Parser in Python successfully parsed %s' % fn)
+    except Exception as e:
+        failures +=1
+        s = ' %s:' % type(e).__name__
+        sys.stderr.write('Failed:%s %s\n' % (s, e))
+        traceback.print_exc()
 
 
 def process(options):
@@ -41,10 +48,6 @@ def process(options):
 def main():
     fn = os.path.basename(__file__)
     fn = os.path.splitext(fn)[0]
-    lfn = os.path.expanduser('~/logs/%s.log' % fn)
-    if os.path.isdir(os.path.dirname(lfn)):
-        logging.basicConfig(level=logging.DEBUG, filename=lfn, filemode='w',
-                            format='%(message)s')
     adhf = argparse.ArgumentDefaultsHelpFormatter
     ap = argparse.ArgumentParser(formatter_class=adhf, prog=fn)
     aa = ap.add_argument
@@ -59,12 +62,7 @@ if __name__ == '__main__':
         rc = main()
     except KeyboardInterrupt:
         rc = 2
-    except Exception as e:
-        if DEBUGGING:
-            s = ' %s:' % type(e).__name__
-        else:
-            s = ''
-        sys.stderr.write('Failed:%s %s\n' % (s, e))
-        if DEBUGGING: import traceback; traceback.print_exc()
-        rc = 1
-    sys.exit(rc)
+    print('\nParsed ' + successes.__str__() + ' Java source files successfully')
+    print('Failed on ' + failures.__str__() + ' files.')
+    print('Duration: ' + (time.time()-start).__str__() + ' seconds.\n')
+sys.exit(rc)
