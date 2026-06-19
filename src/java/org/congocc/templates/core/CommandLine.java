2f8d486585b1a7d01579605f17645b0bcf3d9e72
Jonathan Revusky <revusky@gmail.com>
2026-06-19 08:28:53 +0200

    Mostly fixing up the build.xml. I noticed that there is no gain to the parallel generation of the parsers, so I went back to a simpler scheme. I also decided to include LICENSE file in the jar. Also, fixed up the target for generating congo-templates.jar

--- src/java/org/congocc/templates/core/CommandLine.java
+++ src/java/org/congocc/templates/core/CommandLine.java
@@ -9,13 +9,13 @@ import java.util.HashMap;
  * If invoked with no parameters it just prints the version number.
  * If you invoke it with filename, it reads int the file as a template
  * and processes it with an empty data model, sending the output to stdout.
- * 
+ *
  * Note that this command-line utility mostly exists as a convenient entry
  * point for debugging/testing when setting up a development environment.
  */
 public class CommandLine {
-	
-	
+
+
 	public static void main(String[] args) throws IOException {
 		if (args.length == 0) {
 		    info();
@@ -23,7 +23,7 @@ public class CommandLine {
 		}
 		processTemplate(args[0]);
 	}
-	
+
 	/**
 	 * processes template with an empty data model
 	 * @param filename name of file
@@ -37,16 +37,16 @@ public class CommandLine {
 		Writer out = new OutputStreamWriter(System.out);
 		template.process(new HashMap<String, Object>(), out);
 	}
-    
+
     public static void info() {
         System.out.println();
         System.out.print("Congo Templates version ");
         System.out.println(Configuration.getVersionNumber());
         System.out.println();
-        System.out.println("Copyright (c) 2023 Jonathan Revusky.");
+        System.out.println("Copyright (c) 2023-2026 Jonathan Revusky.");
         System.out.println("All rights reserved.");
         System.out.println();
-        System.out.println("This is Free software. Please read the LICENSE.md");
+        System.out.println("This is Free software. Please read the LICENSE");
         System.out.println("for more details.");
         System.out.println();
         System.out.println("For more information and for updates visit:");
